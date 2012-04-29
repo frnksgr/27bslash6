fabcc06ce86af66f93f4715d8013793f9d48f14b
Frank Seeger <frnksgr@gmail.com>
2012-04-29 17:00:52 +0000

    fixes

--- http_simple.js
+++ http_simple.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-var app = module.exports = express.CreateServer();
+var app = module.exports = express.createServer();
 
 app.get("/hello", function (req, res) {
         res.send("Hello, World!");
