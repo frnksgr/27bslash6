f9e16079879eaa8e495d9d33f9e482717c617399
Frank Seeger <frnksgr@gmail.com>
2012-04-29 17:10:06 +0000

    added Procfile

--- http_simple.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-
-var app = module.exports = express.createServer();
-
-app.get("/hello", function (req, res) {
-        res.send("Hello, World!");
-});
--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-var app = require("./http_simple");
+var app = require("./app");
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {    
