ef571eed8b4e1890e039a33adbd18a37beeb9470
Frank Seeger <frnksgr@gmail.com>
2012-04-29 16:38:09 +0000

    foo

--- web.js
+++ web.js
@@ -1,4 +1,7 @@
 var app = require("http_simple");
 var port = process.env.PORT || 3000;
 
-app.listen(port);
+app.listen(port, function() {    
+    var addr = app.address()
+    console.log("server on http://" + addr.address + ":" + addr.port);
+});
