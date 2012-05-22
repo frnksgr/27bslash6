67948a1f518b5cd60369c14f8fb89e59504b09bc
Frank Seeger <frnksgr@gmail.com>
2012-05-22 16:51:01 +0200

    added module

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require("express");
+var mod = require("27bslash6-module");
+var app = express.createServer();
 
-var app = module.exports = express.createServer();
+exports = module.exports = app;
 
 app.get("/", function (req, res) {
-        res.send("Hello, World!"); 
+    res.send("Hello, " + mod.text);
 }); 
+
--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-var app = require("./app");
+var app = require("./app.js");
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {    
